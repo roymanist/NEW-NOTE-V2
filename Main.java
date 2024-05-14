d82dde42c9a74dbb990024fdb017e3ed63e3cdaa
Nikita Kurhanov <gag7023@gmail.com>
2024-05-14 12:56:18 +0300

    Первое добавление мейна

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args) {
+        // Ваш код начинается здесь
+        System.out.println("Привет, мир!");
+        // Ваш код заканчивается здесь
+    }
+}
