836ec42270103ddffe3c4dce044c07a3affa44f0
Nikita Kurhanov <gag7023@gmail.com>
2024-05-16 17:26:45 +0300

    Установил в проэкт мавен и ломбок

--- services/NoteManager.java
+++ services/NoteManager.java
@@ -1,15 +1,5 @@
 package services;
 
-import obj.Note;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class NoteManager implements IReadNoteBase, IDrawNotes, INewNote, IDeleteNote, IUpdateNote {
 
 }
